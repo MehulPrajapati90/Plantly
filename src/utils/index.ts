137ddaabee39bd4196207c0dfa44e4f4241ab721
Mehul Prajapati <ailearn90@gmail.com>
2025-11-10 03:25:39 +0530

    added preview on workspace

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+import { Instagram, Linkedin, Mail, Twitch, Twitter, Youtube } from "lucide-react";
+
+export const socialPlatforms = [
+    { value: "instagram", label: "Instagram", icon: Instagram },
+    { value: "youtube", label: "YouTube", icon: Youtube },
+    { value: "email", label: "Email", icon: Mail },
+    { value: "Twitter", label: "Twitter", icon: Twitter },
+    { value: "Twitch", label: "Twitch", icon: Twitch },
+    { value: "Linkedin", label: "Linkedin", icon: Linkedin },
+]
