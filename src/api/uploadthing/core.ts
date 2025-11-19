7a57ab918ea6c7377e0e7a53595049eaebb47f34
Mehul Prajapati <ailearn90@gmail.com>
2025-11-20 00:48:23 +0530

    added upload-things as object store

---
+++ src/api/uploadthing/core.ts
@@ -0,0 +1,59 @@
+import { client } from "@/lib/db";
+import { z } from "zod";
+import { currentUser } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+
+    ProfileImageUploader: f({
+        image: {
+            maxFileSize: "4MB",
+            maxFileCount: 1
+        }
+    })
+        .middleware(async () => {
+            const user = await currentUser();
+
+            return { user: user }
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            await client.user.update({
+                where: {
+                    clerkId: metadata.user?.id
+                },
+                data: {
+                    imageUrl: file.url
+                }
+            })
+
+            return { fileUrl: file.url }
+        }),
+
+    UploadLinkUploader: f({
+        image: {
+            maxFileSize: "4MB",
+            maxFileCount: 1
+        }
+    })
+        .input(z.object({
+            id: z.string()
+        }))
+        .middleware(async ({ input }) => {
+            const user = await currentUser();
+
+            return { user: user, id: input?.id };
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            // TODO: Update the link with the uploaded file URL
+            if (!metadata.id) {
+                throw new UploadThingError("No link ID provided in metadata");
+            }
+        })
+
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
+export type UploadLinkUploader = typeof ourFileRouter;
---
+++ src/lib/uploadThings.ts
@@ -0,0 +1,5 @@
+import { UploadButton, UploadDropzone, Uploader } from "@uploadthing/react";
+import type { OurFileRouter } from "@/api/uploadthing/core";
+
+export { UploadButton, UploadDropzone, Uploader };
+export type { OurFileRouter };
