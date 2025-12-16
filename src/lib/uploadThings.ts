88bdca9357704457632467404c88e0d7c877205f
Mehul Prajapati <ailearn90@gmail.com>
2025-12-16 21:53:46 +0530

    added openfilerouter type

--- src/lib/uploadThings.ts
+++ src/lib/uploadThings.ts
@@ -2,4 +2,4 @@ import { UploadButton, UploadDropzone, Uploader } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export { UploadButton, UploadDropzone, Uploader };
-export type { OurFileRouter };
+export type { OurFileRouter }; 
